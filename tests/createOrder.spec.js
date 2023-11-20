6d2c47d912892962097dfa3c805bca366c8a21c9
Abishri765 <beingshriabi@gmail.com>
2023-11-20 17:48:06 +0530

    first commit

--- tests/createOrder.spec.js
+++ tests/createOrder.spec.js
@@ -1,20 +1,30 @@
+
 import { test, expect } from '@playwright/test';
 
+
 test('test', async ({ page }) => {
-  await page.goto('https://app.facilio.com/identity/login');
+
+ await page.goto('http://localhost:8080/auth/login');
+
   await page.getByPlaceholder('Enter your email address').click();
-  await page.getByPlaceholder('Enter your email address').fill('abishri+13june23@facilio.com');
+
+  await page.getByPlaceholder('Enter your email address').fill('test@facilio.com');
+
   await page.getByRole('button', { name: 'Next' }).click();
-  await page.getByPlaceholder('Enter password').click();
-  await page.getByPlaceholder('Enter password').fill('Test1235');
-  await page.getByRole('button', { name: 'Sign in', exact: true }).click();
-  await expect(page).toHaveTitle("Facilio");
-  await page.locator('#sidebar-icon-12113').click();
-  await page.getByRole('button', { name: 'New Work Order' }).first().click();
-  await page.locator('.fc-input-full-border2 > .el-input__inner').click();
-  await page.locator('.fc-input-full-border2 > .el-input__inner').fill('New');
-  await page.locator('.el-select > .el-input > .el-input__inner').first().click();
-  await page.locator('span').filter({ hasText: 'Facilio technology' }).click();
-  await page.getByRole('main').click();
-  await page.locator('body').press('Escape');
+
+  await page.getByPlaceholder('Enter your password').fill('1234');
+
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  console.log(await page.viewportSize().width);
+  console.log(await page.viewportSize().height);
+
+
+  await page.locator('#sidebar-icon-51').click();
+
+  await page.getByRole('button', { name: 'New Work Order' }).click();
+
+  await page.locator('.fc-input-full-border2 > .el-input__inner').first().click();
+  await page.locator('.fc-input-full-border2 > .el-input__inner').fill('Repairing in AC');
+  await expect(page).toHaveTitle("Workorder - Facilio")
+
 });
